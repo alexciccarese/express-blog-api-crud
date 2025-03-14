3dbbfe9043a696e9683a0fc67bcf7740260df9b6
alexciccarese <alex.ciccarese@gmail.com>
2025-03-14 15:13:25 +0100

    add image, data with arrey and posts router

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3003
+const postsRouter = require('./routers/posts')
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+})
+
+app.get('/', (req, res) => {
+  
+  res.send('Server del mio blog')
+})
+
+app.use("/posts", postsRouter)
