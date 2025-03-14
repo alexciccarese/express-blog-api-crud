46e9638dbb65063e75b5fadb093115715cd1afc5
alexciccarese <alex.ciccarese@gmail.com>
2025-03-14 15:22:43 +0100

    add posts controller

---
+++ controllers/postsController.js
@@ -0,0 +1,34 @@
+const menu = require('../data/menu')
+
+function index(req, res) {
+  res.json(posts)
+}
+
+function show(req, res) {
+  res.send(`Return post with id: ${postId}`)
+}
+
+function store(req, res) {
+  res.send('Store a new post')
+}
+
+function update(req, res) {
+  res.send(`Update the post with an id of ${req.params.id}`)
+}
+
+function modify(req, res) {
+  res.send(`Modify the post with an id: ${req.params.id}`)
+}
+
+function destroy(req, res) {
+  res.send(`Delete the post with an id of ${req.params.id}`)
+}
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  modify,
+  destroy
+}
