bc69dc1afe52a2a9c6db6a2cee7e4c16fca1bda0
alexciccarese <alex.ciccarese@gmail.com>
2025-03-17 15:10:49 +0100

    add body parsr

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express()
 const port = 3003
 const postsRouter = require('./routers/posts')
 
+//rgistro il body-parsr pr "application/jason"
+app.use(express.json())
+
+app.post("/", (req, res) => {
+  //dentro req.body troveremo
+  //i dati ricevuti in formato json
+  console.log(req.body);
+  
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 })
