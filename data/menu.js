8b7909b57fed8d91b3aee54990a5b5774a314fba
alexciccarese <alex.ciccarese@gmail.com>
2025-03-28 17:15:06 +0100

    update image paths in menu data to use absolute URLs

--- data/menu.js
+++ data/menu.js
@@ -3,35 +3,35 @@ const posts = [
     title: 'Ciambellone',
     slug: 'ciambellone',
     content: 'Soft anf fluffy cake',
-    image: './images/ciambellone.jpeg',
+    image: '/images/ciambellone.jpeg',
     tags: ['easy-recipe', 'homemade', 'fluffy', 'breakfast', 'snack']
   },
   {
     title: 'Cracker barbabietola',
     slug: 'cracker-barbabietola',
     content: 'Crispy and flavorful cracker',
-    image: './images/cracker_barbabietola.jpeg',
+    image: '/images/cracker_barbabietola.jpeg',
     tags: ['healthy-snack', 'homemade', 'crispy', 'vegan', 'gluten-free']
   },
   {
     title: 'Pane fritto dolce',
     slug: 'pane-fritto-dolce',
     content: 'Golden and crispy sweet fried bread',
-    image: './images/pane_fritto_dolce.jpeg',
+    image: '/images/pane_fritto_dolce.jpeg',
     tags: ['sweet-treat', 'crispy', 'homemade', 'breakfast', 'dessert']
   },
   {
     title: 'Pasta barbabietola',
     slug: 'pasta-barbabietola',
     content: 'Vibrant and delicious beetroot pasta',
-    image: './images/pasta_barbabietola.jpeg',
+    image: '/images/pasta_barbabietola.jpeg',
     tags: ['homemade', 'colorful', 'healthy', 'vegetarian', 'dinner']
   },
   {
     title: 'Torta paesana',
     slug: 'torta-paesana',
     content: 'A rich and rustic bread pudding cake',
-    image: './images/torta_paesana.jpeg',
+    image: '/images/torta_paesana.jpeg',
     tags: ['traditional', 'italian-dessert', 'rustic', 'chocolate', 'homemade']
   },
 ]
